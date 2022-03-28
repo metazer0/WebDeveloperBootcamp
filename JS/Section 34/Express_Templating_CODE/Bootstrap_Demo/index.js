b8e9ca761cfdabc390757bd555247cb8e4068512
Julio Isaac David Ramos <isaacdvid15.96@gmail.com>
2022-03-27 22:29:06 -0600

    updated

--- JS/Section 34/Express_Templating_CODE/Bootstrap_Demo/index.js
+++ JS/Section 34/Express_Templating_CODE/Bootstrap_Demo/index.js
@@ -1,39 +1,43 @@
-const express = require('express');
+const express = require('express')
+const redditData = require('./data.json')
 const app = express();
-const path = require('path');
-const redditData = require('./data.json');
+const path = require('path')
 
-app.use(express.static(path.join(__dirname, 'public')))
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, '/views'))
+app.use(express.static('public'))
 
-app.get('/', (req, res) => {
-    res.render('home')
+
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname,'/views'))
+
+app.get('/',(req,res) => {
+    res.render('home',{name:'home'})
 })
 
-app.get('/cats', (req, res) => {
+app.get('/cats', (req,res)=>{
     const cats = [
-        'Blue', 'Rocket', 'Monty', 'Stephanie', 'Winston'
+        'Blue','Rocket,','Monty','Stephanie','Winston'
     ]
-    res.render('cats', { cats })
+
+    res.render('cats',{cats})  
 })
 
-app.get('/r/:subreddit', (req, res) => {
-    const { subreddit } = req.params;
-    const data = redditData[subreddit];
-    if (data) {
-        res.render('subreddit', { ...data });
-    } else {
-        res.render('notfound', { subreddit })
+app.get('/r/:subreddit',(req,res) => {
+    const {subreddit} = req.params
+    const data = redditData[subreddit]
+    if(data){
+        res.render('subreddit',{...data, name:`${subreddit}`})
+    }else{
+        res.render('notfound', {subreddit, name:'not found'})
     }
 })
 
-app.get('/rand', (req, res) => {
-    const num = Math.floor(Math.random() * 10) + 1;
-    res.render('random', { num })
+app.get('/rand',(req,res)=>{
+    const num = Math.floor(Math.random()*10)+1
+    res.render('random', {rand : num, name:'random'})
 })
 
 app.listen(3000, () => {
-    console.log("LISTENING ON PORT 3000")
+    console.log("listening on port 3000")
 })
