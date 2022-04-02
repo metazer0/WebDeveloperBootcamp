e37dde9055d90c951d1c980af4991a72d3b45eb7
Julio Isaac David Ramos <isaacdvid15.96@gmail.com>
2022-04-01 22:15:31 -0600

    update

--- JS/Section 35/RESTful_Routes_CODE/RESTDemo/index.js
+++ JS/Section 35/RESTful_Routes_CODE/RESTDemo/index.js
@@ -1,14 +1,50 @@
 const express = require('express')
 const app = express()
+const path = require('path')
+
+app.use(express.urlencoded({extended:true}))
+
+app.use(express.json());
+
+app.set('views', path.join(__dirname,'views'))
+app.set('view engine', 'ejs')
+
+const comments = [
+
+    {
+        username:"Todd",
+        comment:"LMAO"
+    },
+    {
+        username:"Skyler",
+        comment:"LOL"
+    },
+    {
+        username:"MikeLitoris",
+        comment:"ROFL"
+    },
+    {
+        username:"R34",
+        comment:"XD"
+    }
+
+]
+//index to render multiple resources
+app.get('/comments',(req,res)=>{
+    
+    res.render('comments/index',{comments})
+})
 
 app.get('/tacos',(req,res)=>{
     res.send('GET /tacos response')
 })
 
 app.post('/tacos',(req,res)=>{
-    res.send('POST /tacos response')
+    const {meat, qty} = req.body;
+    res.send(`OK, HERE ARE YOUR ${qty} ${meat} TACOS!`)
 })
 
 app.listen(3000,()=>{
     console.log('on port 3000')
 })
+
